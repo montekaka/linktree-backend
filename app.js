1cfb1c6ff13ffc93e910136c59380e6d5134c7dd
montekaka <josh.chen.jia@gmail.com>
2021-11-25 11:31:09 -0800

    add routes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const port = 5000;
 const app = express();
 app.use(express.json());
 
+require('./routes')(app); // connect routes
+
 app.listen({port}, async () => {
   console.log(`listening on port ${port}!`);
-  await sequelize.sync();
+  await sequelize.sync(); // connect database
   console.log('Database synced!')
 })

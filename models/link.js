20ee35dfe052f4a8d72482253648e65c4bed3e65
montekaka <josh.chen.jia@gmail.com>
2021-11-25 10:48:35 -0800

    adds relationship between user and link, user has many links, link belongs to user

--- models/link.js
+++ models/link.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(User, {foreignKey: 'userId', onDelete: 'CASCADE'})
     }
   };
   Link.init({
--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(Link, {foreignKey: 'userId'})
     }
   };
   User.init({

ab9b89fc59f5fe1c3fe50482f49dfadbcd884454
montekaka <josh.chen.jia@gmail.com>
2021-11-25 11:23:53 -0800

    adds app.js file to start the server and connect to database

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = 5000;
+const app = express();
+app.use(express.json());
+
+app.listen({port}, async () => {
+  console.log(`listening on port ${port}!`);
+  await sequelize.sync();
+  console.log('Database synced!')
+})

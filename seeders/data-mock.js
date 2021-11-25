d8bccfab5afb7bccbdb3fda909ac84871f766b65
montekaka <josh.chen.jia@gmail.com>
2021-11-25 10:39:30 -0800

    adds mock data for users

---
+++ seeders/data-mock.js
@@ -0,0 +1,5 @@
+const users = [
+  {id: 1, email: "abc@abc.com", name: "abc", createdAt: new Date(), updatedAt: new Date()},
+  {id: 2, email: "111@abc.com", name: "111", createdAt: new Date(), updatedAt: new Date()},
+  {id: 3, email: "xyz@abc.com", name: "xyz", createdAt: new Date(), updatedAt: new Date()}
+]

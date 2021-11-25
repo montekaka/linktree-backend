e990aff36b57f82bc2912c8e19b00afeb4e0f5ad
montekaka <josh.chen.jia@gmail.com>
2021-11-25 12:31:46 -0800

    changes the type name to match with the readme document

--- mock_data/index.js
+++ mock_data/index.js
@@ -10,15 +10,15 @@ const users = [
 // - type: string (TODO: create a table to store types)	
 
 const links = [
-  {userId: 1, type:"classic_links", url: "https://www.yahoo.com", title: "Yahoo!", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 2, type:"classic_links", url: "https://www.google.com", title: "Google", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 3, type:"classic_links", url: "https://www.duckduckgo.com", title: "DuckDuckGo", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 1, type:"show_lists", url: "https://www.hbo.com/last-week-tonight-with-john-oliver", title: "Tonight with John Oliver", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 2, type:"show_lists", url: "https://www.bts.com", title: "BTS Live", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 3, type:"show_lists", url: "https://www.kanye.com", title: "Kanye Live", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 1, type: "music_players", title: "1989", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 2, type: "music_players", title: "Acquired Podcast", createdAt: new Date(), updatedAt: new Date()},
-  {userId: 3, type: "music_players", title: "&Twice", createdAt: new Date(), updatedAt: new Date()}
+  {userId: 1, type:"classic", url: "https://www.yahoo.com", title: "Yahoo!", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 2, type:"classic", url: "https://www.google.com", title: "Google", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 3, type:"classic", url: "https://www.duckduckgo.com", title: "DuckDuckGo", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 1, type:"shows_list", url: "https://www.hbo.com/last-week-tonight-with-john-oliver", title: "Tonight with John Oliver", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 2, type:"shows_list", url: "https://www.bts.com", title: "BTS Live", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 3, type:"shows_list", url: "https://www.kanye.com", title: "Kanye Live", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 1, type: "music_player", title: "1989", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 2, type: "music_player", title: "Acquired Podcast", createdAt: new Date(), updatedAt: new Date()},
+  {userId: 3, type: "music_player", title: "&Twice", createdAt: new Date(), updatedAt: new Date()}
 ]
 
 module.exports = {

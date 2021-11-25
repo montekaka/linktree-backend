60d78ae5b3d5cd73486905ebc4474191bda946cf
montekaka <josh.chen.jia@gmail.com>
2021-11-25 10:41:34 -0800

    adds export users data

--- seeders/data-mock.js
+++ seeders/data-mock.js
@@ -3,3 +3,8 @@ const users = [
   {id: 2, email: "111@abc.com", name: "111", createdAt: new Date(), updatedAt: new Date()},
   {id: 3, email: "xyz@abc.com", name: "xyz", createdAt: new Date(), updatedAt: new Date()}
 ]
+
+
+module.exports = {
+  users
+}

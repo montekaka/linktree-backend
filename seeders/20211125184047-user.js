dada3b499fef4bbe54422c8257421f2c5bf82a1d
montekaka <josh.chen.jia@gmail.com>
2021-11-25 10:43:04 -0800

    adds user seed file

---
+++ seeders/20211125184047-user.js
@@ -0,0 +1,12 @@
+'use strict';
+const {users} = require('./data-mock');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', users, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};

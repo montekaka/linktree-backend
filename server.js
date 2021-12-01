cd3b269fdb0580aa55e13f05ed67a6c7094cb77e
montekaka <josh.chen.jia@gmail.com>
2021-12-01 10:32:21 -0800

    adds pg in docker;

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const {sequelize} = require('./models');
 const {app} = require('./app')
 
 const port = process.env.PORT || 8080;
+let retires = 5; // retry to connect to database
 
 app.listen({port}, async () => {
   console.log(`listening on port ${port}!`);
   await sequelize.sync(); // connect database
   console.log('Database synced!')
+  // while(retires) {
+  //   try {
+  //     await sequelize.sync(); // connect database
+  //     console.log('Database synced!')
+  //     break;
+  //   } catch (err) {
+  //     console.log(err);
+  //     retires -= 1;
+  //     console.log(`retires left: ${retires}`);
+  //     // wait 5 seconds
+  //     await new Promise(res => setTimeout(res, 5000))
+  //   }
+  // }
+
 })
